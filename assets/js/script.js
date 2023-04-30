5315aea223b36fa63d0e18507000d44feef6ce18
dinakidane <dinakidane@hotmail.com>
2023-04-30 16:47:13 +0000

    create const variables for elements

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+
+/**
+ * Const Variables
+ * Create variables that represents elements in our document
+ * these variables will be used to access the elements via DOM model
+ */
+
+const restartBtn = document.getElementById("restart");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const submitBtn = document.getElementById("submit");
+const trueBtn = document.getElementById("True");
+const falseBtn = document.getElementById("False");
+const userScore = document.getElementById("user-score");
+const totalScore = document.getElementById("total-score");
+const questionText = document.getElementById("question-text");
+
